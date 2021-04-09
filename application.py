9fa03c641b96c70f259da67ac0c3eede2a24a4e4
akang-Gendang <76055268+akang-Gendang@users.noreply.github.com>
2021-04-09 03:40:47 +0200

    Add files via upload

--- application.py
+++ application.py
@@ -3,41 +3,27 @@ import random
 from movie import quotee, movie_list
 application = Flask(__name__)
 
-posts = [
-    {
-        'author': 'Me',
-        'title' : 'Kura kura tidak tahu',
-        'content': 'No content currently',
-        'date'  : 'February 14, 2021'
-    },
-    {
-        'author': 'Not Me',
-        'title' : 'Kura kura sudah tahu',
-        'content': 'Some content added',
-        'date'  : 'February 14, 2021'
-    }
-        
-]
-
-# {{ url_for('static', filename='js/scripts.js') }}pi
+# route
 @application.route('/movie')
 def hello_world():
     return '<div  style="Background-color: Green"><h1> Hello, World! </h1> </div>'
 
+@application.route('/home')
+def home():
+    return '<body style="background-color: #fde0d9; padding-top:30px padding-bottom:57px; font-size:30px" > <h2> Hello from Better Call Saul-quote! </h2></body>\
+    <footer  class="bg-light py-5">\
+            <div class="container"><div class="small text-center text-muted">Copyright © 2020 - Zaki Muhammad</div></div>\
+        </footer>'
 
 @application.route('/new')
 def new():
-    return render_template('layoutnew.html')
-
-@application.route('/home')
-def home():
-    return render_template('index.html')
+    return render_template('home.html')
 
 @application.route('/')
 def movie():
     number = random.randint(0, 11)
     num    = 2
-    if number < 4:
+    if number < 5:
         num = 0
     quote  = quotee[number]
     tit    = movie_list[num]
